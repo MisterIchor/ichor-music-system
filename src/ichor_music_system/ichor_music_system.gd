02eac51879cc4d04aaf46ca33494a7436066f064
MisterIchor <superluigibrosforever@gmail.com>
2025-06-05 13:15:51 -0400

    flexible_music_system.gd: Renamed to ichor_music_system.gd. Moved to folder of same name within src.

--- src/ichor_music_system/ichor_music_system.gd
+++ src/ichor_music_system/ichor_music_system.gd
@@ -10,9 +10,11 @@ static var current_song: String = "":
 	set = set_current_song
 static var folder_path: String = "":
 	set = set_folder_path
-static var music_audio_bus: String = "":
-	set = set_music_audio_bus
+static var audio_bus: String = "":
+	set = set_audio_bus
 static var pause_between_songs: float = 0.05
+static var fade_in: float = 0.025
+static var fade_out: float = 0.025
 static var is_stopped: bool = false:
 	set = set_is_stopped
 static var is_paused: bool = false:
@@ -21,10 +23,19 @@ static var is_paused: bool = false:
 static var _song_list: Array = []
 static var _playback: AudioStreamSynchronized = AudioStreamSynchronized.new()
 static var _stream: AudioStreamPlayback = AudioStreamPlayback.new()
+static var _song_position: float = 0.00
 
 
 
 static func play() -> void:
+	return
+
+
+static func pause() -> void:
+	return
+
+
+static func stop() -> void:
 	return
 
 
@@ -39,3 +50,24 @@ static func play_next() -> void:
 
 static func shuffle_song_list() -> void:
 	_song_list.shuffle()
+
+
+
+static func set_current_song(new_song: String) -> void:
+	return
+
+
+static func set_folder_path(new_path: String) -> void:
+	return
+
+
+static func set_audio_bus(bus_name: String) -> void:
+	return
+
+
+static func set_is_stopped(value: bool) -> void:
+	return
+
+
+static func set_is_paused(value: bool) -> void:
+	return
